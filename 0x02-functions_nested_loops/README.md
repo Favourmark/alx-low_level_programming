5c319ceaeab80a646f6bf888a27c8dd23c266ef2
Favourmark <favourogonnamark@gmail.com>
2023-08-16 05:46:06 -0700

    0-putchar.c

--- 0x02-functions_nested_loops/README.md
+++ 0x02-functions_nested_loops/README.md
@@ -1,1 +1,1 @@
-0x02. C - Functions, nested loops readme file
+0x02-functions_nested_loops readme file
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,5 +1,6 @@
-#ifndef MAIN_HEADER _FILE
+#ifndef MAIN_HEADER_FILE
 #define MAIN_HEADER_FIILE
+
 int _putcher(char c);
 void print_alphabet(void);
 void print_alphabet_x10(void);
@@ -12,4 +13,6 @@ void jack_bauer(void);
 void times_table(void);
 int add(int, int);
 void print_to_98(int n);;
+void print_times_table(int n);
+
 #endif
